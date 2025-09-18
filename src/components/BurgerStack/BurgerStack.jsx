a74bfe49affe4ffb4438140e1ebe9867dbc40427
Rtkmak14 <Rtkmak14@gmail.com>
2025-09-18 09:59:45 -0500

    lifting states lab submission

---
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -0,0 +1,17 @@
+// src/components/BurgerStack/BurgerStack.jsx
+
+const BurgerStack = (props) => { 
+  return (
+    <ul>
+      {props.stack.map((ingredient,index)=>(
+        <li key={index} style={{ backgroundColor: ingredient.color }}>
+          {ingredient.name}
+          <button type="submit" onClick={() => props.removeFromBurger(index)}>X</button>
+        </li>
+      ))}
+    </ul>
+  )
+
+}
+
+export default BurgerStack;
